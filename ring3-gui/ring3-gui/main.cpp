35ba102f581eecd23eb9bc98a1c7e928020891ba
Mairon <60795188+ZeroMemoryEx@users.noreply.github.com>
2025-08-21 14:37:03 +0100

    Update main.cpp

--- ring3-gui/ring3-gui/main.cpp
+++ ring3-gui/ring3-gui/main.cpp
@@ -1,5 +1,7 @@
-
 // Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
 #define STB_IMAGE_IMPLEMENTATION
 #define IMGUI_DEFINE_MATH_OPERATORS
 #define _CRT_SECURE_NO_WARNINGS
